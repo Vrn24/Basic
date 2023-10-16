95dacd9fd14572bff937b709cdee171e3557199c
Vrn24 <112547439+Vrn24@users.noreply.github.com>
2023-10-16 15:20:01 +0530

    Add files via upload

---
+++ Basic Input-Output.cpp
@@ -0,0 +1,30 @@
+/*22070123129*/
+/*Varun Pagote*/
+/*Helo World*/
+#include <iostream>
+using namespace std;
+int main()
+{
+int a,b,c,d,e,f,g;
+int sum;
+int subtraction;
+int multiplication;
+cout<<"enter first number";
+cin>>a;
+cout<<"enter second number";
+cin>>b;
+sum = a+b;
+cout<<"the sum of two numbers is"<<sum<<endl;
+cout<<"enter first number"<<endl;
+cin>>d;
+cout<<"enter the second number"<<endl;
+cin>>e;
+subtraction = d-e;
+cout<<"the subtration of two number is"<<subtraction<<endl;
+cout<<"enter the first number";
+cin>>f;
+cout<<"enter the second number";
+cin>>g;
+multiplication = f*g;
+cout<<"the multiplication of two number is"<<multiplication<<endl;
+}
---
+++ Hello World.cpp
@@ -0,0 +1,10 @@
+/*22070123129*/
+/*Varun Pagote*/
+/*Experiment 2*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+
+}
